3fda9a6102be6029cd15a7d31fb55822dcb1d6d0
Ivan Kraev <kukurua.tuni@gmail.com>
2020-11-14 17:10:11 +0300

    add cenbot veryfy route

--- src/controllers/baseController.js
+++ src/controllers/baseController.js
@@ -5,3 +5,7 @@ module.exports.home = (req,res) => {
 module.exports.smoothies = (req,res) => {
     res.render('smoothies')
 }
+
+module.exports.certbot_verify = (req,res) => {
+    res.send('Ew3FiKa_HyTf9jnDkc5S8Kq9ZLdhRoXnkp6YT0bB1Ow.p6gQrgoIGdfp8Uk42890pfkOv3nhX3TKCOqa23pt7_U')
+}
--- src/routes/baseRoutes.js
+++ src/routes/baseRoutes.js
@@ -11,4 +11,7 @@ router.get('*', checkUser) // apply checkUser to all pages so that they have use
 router.get('/', baseController.home)
 router.get('/smoothies', requireAuth, baseController.smoothies)
 
+router.get('/.well-known/acme-challenge/Ew3FiKa_HyTf9jnDkc5S8Kq9ZLdhRoXnkp6YT0bB1Ow', baseController.certbot_verify
+)
+
 module.exports = router
