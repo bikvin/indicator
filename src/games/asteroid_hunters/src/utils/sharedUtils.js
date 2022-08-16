09a9d5559fc19f4e9c1f284edc76adbdaca2e697
Ivan Kraev <kukurua.tuni@gmail.com>
2022-08-16 20:13:42 +0300

    test save topOpenLevel to db 6

--- src/games/asteroid_hunters/src/utils/sharedUtils.js
+++ src/games/asteroid_hunters/src/utils/sharedUtils.js
@@ -14,6 +14,7 @@ export default class SharedUtils{
                 console.log('try save to db')
                 //const astVkLevel = await AstVkLevel.create({vkUserId:1111, topOpenLevel: topOpenLevel })
                 //res.status(201).json({room: room._id})
+                AstVkLevel.create({vkUserId:1111, topOpenLevel: topOpenLevel }).then(()=>{console.log('Saved')})
             }
             catch (err){
                 //res.status(400).send({ err })
