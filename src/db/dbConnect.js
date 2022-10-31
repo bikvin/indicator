7b5e6a1d3590be78d2ccfcd4b7d4bd3029162656
Ivan Kraev <kukurua.tuni@gmail.com>
2022-10-31 14:34:12 +0300

    change mongo to local

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-dbConnect = mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+//dbConnect = mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+dbConnect = mongoose.connect(process.env.MONGODB_URL, {})
   //.then((result) => app.listen(3000))
   .then((result) => console.log("Connected to db"))
   .catch((err) => console.log(err));
