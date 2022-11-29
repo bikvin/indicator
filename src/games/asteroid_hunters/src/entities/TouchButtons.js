08e6684910b063c644d04e430649f15987eca79b
Ivan Kraev <kukurua.tuni@gmail.com>
2022-11-29 21:18:07 +0300

    try make touch buttons better

--- src/games/asteroid_hunters/src/entities/TouchButtons.js
+++ src/games/asteroid_hunters/src/entities/TouchButtons.js
@@ -9,20 +9,33 @@ export default class TouchButtons {
 
         this.scene.input.addPointer(3)
 
-        this.leftButton = this.scene.add.image(50*this.scene.config.scaleMultiplier, this.scene.config.height - 200* this.scene.config.scaleMultiplier, 'left_right_button').setAngle(180).setDepth(2).setScale(1.2*this.scene.config.scaleMultiplier);
+        this.leftButton = this.scene.add.sprite(50*this.scene.config.scaleMultiplier, this.scene.config.height - 200* this.scene.config.scaleMultiplier, 'left_right_button')
+        .setAngle(180)
+        .setDepth(2)
+        .setScale(1.2*this.scene.config.scaleMultiplier);
 
-        this.rightButton = this.scene.add.image(230* this.scene.config.scaleMultiplier, this.scene.config.height - 60* this.scene.config.scaleMultiplier, 'left_right_button').setDepth(2).setScale(1.2* this.scene.config.scaleMultiplier);
+        this.rightButton = this.scene.add.sprite(170* this.scene.config.scaleMultiplier, this.scene.config.height - 60* this.scene.config.scaleMultiplier, 'left_right_button').setDepth(2).setScale(1.2* this.scene.config.scaleMultiplier);
 
-        this.gasButton = this.scene.add.image(this.scene.config.width - 80* this.scene.config.scaleMultiplier, this.scene.config.height - 230* this.scene.config.scaleMultiplier, 'gas_button').setAngle(-90).setDepth(2).setScale(1.2* this.scene.config.scaleMultiplier);
+        this.gasButton = this.scene.add.sprite(this.scene.config.width - 80* this.scene.config.scaleMultiplier, this.scene.config.height - 230* this.scene.config.scaleMultiplier, 'gas_button').setAngle(-90).setDepth(2).setScale(1.2* this.scene.config.scaleMultiplier);
 
-        this.shootButton = this.scene.add.image(this.scene.config.width - 230* this.scene.config.scaleMultiplier, this.scene.config.height - 70* this.scene.config.scaleMultiplier, 'shoot_button').setScale(1* this.scene.config.scaleMultiplier).setDepth(2);
+        this.shootButton = this.scene.add.sprite(this.scene.config.width - 170* this.scene.config.scaleMultiplier, this.scene.config.height - 70* this.scene.config.scaleMultiplier, 'shoot_button').setScale(1* this.scene.config.scaleMultiplier).setDepth(2);
 
         this.controlButtons = [this.leftButton, this.rightButton, this.gasButton, this.shootButton];
 
-        this.leftButton.setInteractive();
-        this.rightButton.setInteractive();
-        this.gasButton.setInteractive();
-        this.shootButton.setInteractive();
+        const shape = new Phaser.Geom.Circle(this.leftButton.width/2, this.leftButton.height/2, this.leftButton.width);
+
+    
+
+
+        this.leftButton.setInteractive(new Phaser.Geom.Circle(this.leftButton.width/2, this.leftButton.height/2, this.leftButton.width), Phaser.Geom.Circle.Contains);    
+        this.rightButton.setInteractive(new Phaser.Geom.Circle(this.rightButton.width/2, this.rightButton.height/2, this.leftButton.width), Phaser.Geom.Circle.Contains);
+        this.gasButton.setInteractive(new Phaser.Geom.Circle(this.gasButton.width/2, this.gasButton.height/2, this.leftButton.width), Phaser.Geom.Circle.Contains);
+        this.shootButton.setInteractive(new Phaser.Geom.Circle(this.shootButton.width/2, this.shootButton.height/2, this.leftButton.width), Phaser.Geom.Circle.Contains);
+
+        // this.scene.input.enableDebug(this.leftButton);
+        // this.scene.input.enableDebug(this.rightButton);
+        // this.scene.input.enableDebug(this.gasButton);
+        // this.scene.input.enableDebug(this.shootButton);
 
         this.leftButton.isDown = false;
         this.rightButton.isDown = false;
